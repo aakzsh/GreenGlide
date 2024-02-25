e3c0a10470e6a543574da6eb88425a24cc72cbf2
Aakash Shrivastava <aakash@Aakashs-MacBook-Air.local>
2024-02-25 13:57:07 +0530

    add blank gameplay files

---
+++ lib/screens/gameplay/singleplayer/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SinglePlayerHome extends StatefulWidget {
+  const SinglePlayerHome({super.key});
+
+  @override
+  State<SinglePlayerHome> createState() => _SinglePlayerHomeState();
+}
+
+class _SinglePlayerHomeState extends State<SinglePlayerHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/widgets/buttons/choose_mode.dart
+++ lib/widgets/buttons/choose_mode.dart
@@ -30,7 +30,7 @@ class _ChooseModeButtonState extends State<ChooseModeButton> {
                         color: AppColors.green,
                         border: Border.all(color: Colors.black, width: 2),
                         borderRadius: BorderRadius.circular(10)),
-                    child:  Center(
+                    child: Center(
                         child: LuckiestGuyText(
                             text: widget.title, fontSize: 15.0))),
                 Shimmer.fromColors(
